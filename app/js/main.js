db6a08a7804b7d2ac348fea13884e279a2b4c29b
Aleks551959 <alekseidudarenko@gmail.com>
2021-09-04 01:18:28 +0300

    responsive-3

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,25 @@ $(function() {
     $('.questions__item-title').on('click', function() {
         $('.questions__item').removeClass('questions__item--active');
         $(this).parent().addClass('questions__item--active');
-
     });
 
+    $('#fullpage').fullpage({
+        // здесь параметры 
+        autoScrolling: true,
+        scrollHorizontally: true,
+        sectionSelector: '.page-section',
+        scrollOverflow: true,
+        anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts'],
+        menu: '#header__nav',
+    });
+    $('.menu__btn').on('click', function() {
+        $('.menu__btn').toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+
+    });
+    $('.menu__list-link').on('click', function() {
+        $('.menu__btn').removeClass('menu__btn--active');
+        $('.menu__list').removeClass('menu__list--active');
+
+    });
 });
